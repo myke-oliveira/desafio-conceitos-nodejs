1561bfcb24b587a7f869d7000a6b4413660b634a
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2020-11-03 05:29:52 -0300

    resolve gostack challenge.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4, validate } = require("uuid");
 
 const app = express();
 
@@ -10,24 +9,87 @@ app.use(cors());
 
 const repositories = [];
 
+function validateId(request, response, next) {
+  const{ id } = request.params;
+  
+  if (!validate(id)) {
+    response.status(400).json({ error: "Invalid repository Id."});
+  }
+
+  return next();
+};
+
+app.use('/repositories/:id', validateId);
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: v4(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.'})
+  }
+
+  const { likes } = repositories[repositoryIndex];
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes,
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.status(201).json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.'})
+  }
+
+  repositories.splice(repositoryIndex, 1)
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.'})
+  }
+  repositories[repositoryIndex].likes++;
+
+  return response.status(201).json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
